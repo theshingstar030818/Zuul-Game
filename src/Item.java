a7bc77676df20b8503c3260985167395fdb17206
tanzeelrana <tanzeelrana@cmail.carleton.ca>
2012-10-16 13:25:15 -0400

    Room Item and CommandWords Class added

---
+++ src/Item.java
@@ -0,0 +1,31 @@
+import java.util.*;
+/**
+ * An Item in an adventure game.
+ * 
+ * @author Tanzeel Rana 
+ * @version 2012.10.15
+ */
+public class Item
+{
+    
+    private String itemDescription;
+    private double itemWeight;
+
+    /**
+     * Constructor for objects of class Item
+     */
+    public Item(String itemDescription, double itemWeight)
+    {
+        this.itemDescription = itemDescription;
+        this.itemWeight = itemWeight;
+    }
+    
+    public String getItemDescription(){
+        return itemDescription ;
+    }
+    
+    public double getItemWeight(){
+        return itemWeight;
+    }
+    
+}
---
+++ src/Room.java
@@ -0,0 +1,94 @@
+import java.util.*;
+/**
+ * Class Room - a room in an adventure game.
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ *
+ * A "Room" represents one location in the scenery of the game.  It is 
+ * connected to other rooms via exits.  The exits are labelled north, 
+ * east, south, west.  For each direction, the room stores a reference
+ * to the neighboring room, or null if there is no exit in that direction.
+ * 
+ */
+public class Room 
+{
+    private String description;
+    private HashMap<String, Room> exits;
+    private ArrayList<Item> items;
+    /**
+     * Create a room described "description". Initially, it has
+     * no exits. "description" is something like "a kitchen" or
+     * "an open court yard".
+     * @param description The room's description.
+     */
+    public Room(String description) 
+    {
+        this.description = description;
+        exits = new HashMap<>();
+        items = new ArrayList<Item>();
+    }
+
+    /**
+     * Define the exits of this room.  Every direction either leads
+     * to another room or is null (no exit there).
+     * @param north The north exit.
+     * @param east The east east.
+     * @param south The south exit.
+     * @param west The west exit.
+     */
+    public void setExits(String direction, Room neighbor) 
+    {
+        exits.put(direction, neighbor);
+    }
+
+    /**
+     * @return The description of the room.
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public String getExitString(){
+        
+        
+        String s = "Exits : " ;
+        Set<String> keys = exits.keySet();
+        for (String exit : keys){
+            s += " " + exit;
+        }
+        
+        return s + "\n";
+
+    }
+
+    /**
+     * Return a long description of this room, of the form :
+     * you are in the kitchen 
+     * Exits : north west
+     * @return A description of the room, include exits
+     * 
+     */
+    public String getLongDescription(){
+        return ("You are at the " + description + ".\n" + getExitString() +
+        getItemString() );
+    }
+
+    public Room getExits(String direction){
+        return exits.get(direction);
+    }
+    
+    public void addItem(Item item){
+        items.add(item);
+    }
+    
+    private String getItemString(){
+        String itemString = "Items in room : ";
+        for(Item item : items){
+            itemString += item.getItemDescription() + " " + item.getItemWeight() + "\n" ;
+        }
+        return itemString;
+    }
+    
+}
