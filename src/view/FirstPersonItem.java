ad6fd26558b6db9da3901c08f7e9c553a8ec60ce
ethanaubuchonschool <not_me_37@192.168.2.1>
2012-11-19 20:19:26 -0500

    Added firstperson monster and item classes

---
+++ src/view/FirstPersonItem.java
@@ -0,0 +1,20 @@
+package view;
+
+import model.object.Item;
+
+public class FirstPersonItem extends Item{
+	private String image;
+	
+	public FirstPersonItem(String itemName, double itemWeight, String image) {
+		super(itemName, itemWeight);
+		setImage(image);
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = new String("/img/firstperson/item"+image);
+	}
+}
